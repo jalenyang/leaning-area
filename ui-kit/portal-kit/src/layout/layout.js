9b356cb956a18d1a7547ea0864488e75a18c7508
jalenyang <jalen.yang@qq.com>
2022-08-12 14:04:53 +0800

    update the layout

--- ui-kit/portal-kit/src/layout/layout.js
+++ ui-kit/portal-kit/src/layout/layout.js
@@ -2,23 +2,53 @@ import React from "react";
 import "./layout.less";
 
 class SideBar extends React.Component {
-  render() {
-    return (<div className="sidebar">SideBar</div>);
+  constructor(props) {
+    super(props);
   }
+
+  render() {
+    return (<div className="sidebar">
+      <ul>
+        <li><a title="BaiDu Map" onClick={() => this.props.onClick("https://map.baidu.com/")}>BaiDu Map</a></li>
+        <li><a title="Stock" onClick={() => this.props.onClick("http://q.10jqka.com.cn/")}>Stock</a></li>
+        <li><a title="Weather radar" onClick={() => this.props.onClick("http://www.weather.com.cn/radar/")}>Weather
+          radar</a></li>
+      </ul>
+    </div>);
+  }
+
 }
 
 class Content extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return (<div className="content">Content</div>);
+    return (<div className="content">
+      <iframe src={this.props.source} width="100%" height="100%"></iframe>
+    </div>);
   }
 }
 
 class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      source: "https://map.baidu.com/"
+    };
+  }
+
   render() {
     return (<div className="layout">
-      <SideBar></SideBar>
-      <Content></Content>
+      <SideBar onClick={(source) => this.handleClick(source)}></SideBar>
+      <Content source={this.state.source}></Content>
     </div>);
+  }
+
+
+  handleClick(source) {
+    this.setState({ source: source });
   }
 }
 
